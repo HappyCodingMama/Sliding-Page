0b5ec1215aad93568d854abad92695ad23a48802
HappyCodingMama <happycodingmama@gmail.com>
2020-09-11 01:07:24 +0900

    build the automatic sliding-page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const slides = Array.from(document.querySelectorAll('.slide'));
 const slider = document.querySelector('.slider');
 const buttons = document.querySelectorAll('.buttons div');
+const dotEl = document.querySelector('.dots');
+let timeoutId;
 
 function getNextPrev() {
   const activeSlide = document.querySelector('.slide.active');
@@ -36,12 +38,106 @@ function getPosition() {
     } else {
       slide.style.transform = 'translate(100%)';
     }
+
+    slide.addEventListener('transitionend', () => {
+      slide.classList.remove('top');
+    })
   })
 }
 getPosition();
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-
+    if (button.classList.contains('next')) {
+      getNextSlide();
+    } else if (button.classList.contains('prev')) {
+      getPrevSlide();
+    }
   })
 })
+
+function getNextSlide() {
+  clearTimeout(timeoutId);
+  const current = document.querySelector('.slide.active');
+  const [next, prev] = getNextPrev();
+
+  if (current.classList.contains('top')) {
+    return;
+  }
+  current.classList.add('top');
+  next.classList.add('top');
+  current.classList.remove('active')
+  current.style.transform = 'translate(-100%)';
+  next.classList.add('active')
+  next.style.transform = 'translate(0)';
+  getPosition();
+  getActiveDot();
+  autoLoop();
+}
+function getPrevSlide() {
+  clearTimeout(timeoutId)
+  const current = document.querySelector('.slide.active');
+  const [next, prev] = getNextPrev();
+
+  if (current.classList.contains('top')) {
+    return;
+  }
+  current.classList.add('top');
+  prev.classList.add('top');
+  current.classList.remove('active');
+  current.style.transform = 'translateX(100%)';
+  prev.classList.add('active');
+  prev.style.transform = 'translateX(0)';
+  getPosition();
+  getActiveDot();
+  autoLoop();
+}
+
+//dots functionality
+slides.forEach(slide => {
+  const dot = document.createElement('div');
+  dot.classList.add('dot');
+  dotEl.appendChild(dot);
+})
+
+function getActiveDot() {
+  const allDots = document.querySelectorAll('.dots .dot');
+  const activeSlide = document.querySelector('.slide.active');
+  const activeIndex = slides.indexOf(activeSlide);
+
+  allDots.forEach(dot => {
+    dot.classList.remove('active');
+  })
+
+  allDots[activeIndex].classList.add('active');
+}
+
+function functionalDots() {
+  const allDots = document.querySelectorAll('.dots .dot');
+  allDots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+      getDotSlide(index);
+    })
+  })
+}
+
+function getDotSlide(index) {
+  clearTimeout(timeoutId);
+  slides.forEach(slide => {
+    slide.classList.remove('active');
+  });
+  slides[index].classList.add('active');
+  getPosition();
+  getActiveDot();
+  autoLoop();
+}
+
+function autoLoop() {
+  timeoutId = setTimeout(() => {
+    getNextSlide();
+  }, 5000)
+}
+
+getActiveDot();
+functionalDots();
+autoLoop();
